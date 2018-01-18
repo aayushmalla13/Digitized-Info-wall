639c2d71717e2ed54382d7d6091378d8ad4e9a5a
aayushmalla13 <32125193+aayushmalla13@users.noreply.github.com>
2018-01-18 23:11:31 +0545

    Add files via upload

---
+++ doing/Notifications.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Notifications</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="notes.css">
+</head>
+<body>
+
+	<div id="nav">
+		<?php include 'nav.php'; ?>
+	
+	</div>
+	<div class="container">
+		<div class="Notesbox">
+			<div class="row">
+				<div class="col-md-12 col-lg-12">
+						<h2>Notifications</h2>
+						<ul>
+							<li></li>
+
+						</ul>
+
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+
+
+
+			
+		</div>
+	
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+</body>
+</html>
